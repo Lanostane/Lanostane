07c1f9be9afe222f8ba1f2bde4799238d3069789
Flowaria <wnsghgj1234@naver.com>
2023-09-02 00:48:33 +0900

    Added Unsorted Item Access to FastSortedList

--- Assets/Scripts/Shared/Utils/FastSortedList.cs
+++ Assets/Scripts/Shared/Utils/FastSortedList.cs
@@ -17,6 +17,7 @@ namespace Utils
     public sealed class FastSortedList<T>
     {
         private T[] _Cache = Array.Empty<T>();
+        private T[] _UnsortedCache = Array.Empty<T>();
         private readonly List<T> _InternalList = new();
         private bool _HasDirty = false;
         private readonly Func<T, IComparable> _SortFunc;
@@ -40,20 +41,17 @@ namespace Utils
         {
             get
             {
-                if (_HasDirty)
-                {
-                    if (_SortBy == SortBy.DescendingOrder)
-                    {
-                        _Cache = _InternalList.OrderByDescending(_SortFunc).ToArray();
-                    }
-                    else
-                    {
-                        _Cache = _InternalList.OrderBy(_SortFunc).ToArray();
-                    }
-                    _HasDirty = false;
-                }
-
+                TryUpdateCache();
                 return _Cache;
+            }
+        }
+
+        public T[] UnsortedItems
+        {
+            get
+            {
+                TryUpdateCache();
+                return _UnsortedCache;
             }
         }
 
@@ -87,6 +85,23 @@ namespace Utils
         {
             _InternalList.Clear();
             _HasDirty = true;
+        }
+
+        private void TryUpdateCache()
+        {
+            if (_HasDirty)
+            {
+                if (_SortBy == SortBy.DescendingOrder)
+                {
+                    _Cache = _InternalList.OrderByDescending(_SortFunc).ToArray();
+                }
+                else
+                {
+                    _Cache = _InternalList.OrderBy(_SortFunc).ToArray();
+                }
+                _UnsortedCache = _InternalList.ToArray();
+                _HasDirty = false;
+            }
         }
     }
 }
